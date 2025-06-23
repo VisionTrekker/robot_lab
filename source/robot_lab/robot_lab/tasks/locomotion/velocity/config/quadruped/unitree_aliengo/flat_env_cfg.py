8ab0b1c80def0cc578966a45e5b1d816004a19b2
lzhi <18082258131@163.com>
2025-06-23 23:59:35 +0800

    change the flat terrain for aliengo

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/unitree_aliengo/flat_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/quadruped/unitree_aliengo/flat_env_cfg.py
@@ -3,6 +3,7 @@
 
 from isaaclab.utils import configclass
 
+from robot_lab.terrains import FLAT_TERRAINS_CFG
 from .rough_env_cfg import UnitreeAliengoRoughEnvCfg
 
 
@@ -12,17 +13,17 @@ class UnitreeAliengoFlatEnvCfg(UnitreeAliengoRoughEnvCfg):
         # post init of parent
         super().__post_init__()
 
-        # override rewards
-        self.rewards.base_height_l2.params["sensor_cfg"] = None
+        # ------------------------------Sence------------------------------
         # change terrain to flat
-        self.scene.terrain.terrain_type = "plane"
-        self.scene.terrain.terrain_generator = None
-        # no height scan
-        self.scene.height_scanner = None
+        self.scene.terrain.terrain_generator = FLAT_TERRAINS_CFG
+
+        # ------------------------------Observations------------------------------
         self.observations.policy.height_scan = None
-        self.observations.critic.height_scan = None
-        # no terrain curriculum
-        self.curriculum.terrain_levels = None
+
+        # ------------------------------Rewards------------------------------
+        self.rewards.base_height_l2.weight = 0.5
+        self.rewards.base_height_l2.params["target_height"] = 0.40
+        self.rewards.base_height_l2.params["asset_cfg"].body_names = [self.base_link_name]
 
         # If the weight of rewards is 0, set rewards to None
         if self.__class__.__name__ == "UnitreeAliengoFlatEnvCfg":
